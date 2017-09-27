0e9e30a3e17aff78672646b87c2cb543de113e96
philipwisner <philipwisner@gmail.com>
2017-09-27 20:09:05 +0200

    iteration 1 started

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { CinemaService }from './services/cinema-service'
 import { AppComponent } from './app.component';
 
 @NgModule({
---
+++ starter-code/src/app/models/movie.ts
@@ -0,0 +1,15 @@
+export class Movie {
+  id: number;
+  title: string;
+  poster: string;
+  synopsis: string;
+  genres: Array<string>;
+  year: number;
+  director: string;
+  actors: Array<string>;
+  hours: Array<string>;
+  room: number;
+
+  constructor() {
+  }
+}
