6739957f9ea57f1af46ee098af49e2ba4396ee99
philipwisner <philipwisner@gmail.com>
2017-09-27 20:56:43 +0200

    iteration 1 done

--- starter-code/src/app/app.module.ts
+++ starter-code/src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { Routes } from "@angular/router";
 
 import { CinemaService }from './services/cinema-service'
+
 import { AppComponent } from './app.component';
+import { MyHomeComponent } from './components/my-home/my-home.component';
+import { MyMoviesComponent } from './components/my-movies/my-movies.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: MyHomeComponent },
+  { path: 'movies/:id', component: MyMoviesComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyHomeComponent,
+    MyMoviesComponent
   ],
   imports: [
     BrowserModule,
